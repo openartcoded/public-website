8b2898e973fc5201de0901d765b635e925c556f5
Nordine Bittich <nordine@bittich.be>
2024-03-02 18:34:26 +0100

    small refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   content: ["./views/**/*.{html,js}", "./public/**/*.{html,js}"],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    
-  ],
-}
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            blockquote: {
+              color: theme("colors.gray.400"),
+            },
+            a: {
+              color: theme("colors.yellow.500"),
+              textDecoration: "underline",
+            },
+            strong: {
+              color: theme("colors.white"),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
